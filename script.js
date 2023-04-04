3b92da38db77608a2cf6bd12cfc69f7f34b8ba00
levymc <levymcruz@gmail.com>
2023-04-04 19:16:47 -0300

    cards aleatórios fixos

--- script.js
+++ script.js
@@ -4,53 +4,67 @@ let firstCard = null;
 let secondCard = null;
 
 cards.forEach(card => {
-    card.addEventListener('click', () => { //Espera clicar em algum card
+  // Obtem a posição de cada carta
+  const cardRect = card.getBoundingClientRect();
+  const cardInfo = {
+    top: cardRect.top,
+    left: cardRect.left,
+    right: cardRect.right,
+    bottom: cardRect.bottom
+  };
+  
+  card.addEventListener('click', () => { //Espera clicar em algum card
+    const img = card.querySelector("img");
         
-        const randomNum = Math.floor(Math.random() * 8) + 1;
-        const img = card.querySelector("img");
-        
-        if (flippedCards.length < 2 && !card.classList.contains("flipped")) {
-            if (!firstCard) {
-                firstCard = card;
-                card.classList.add("flipped");
-                img.src = `img/${randomNum}.gif`;
-                flippedCards.push(card);
-            }
-            else if (!secondCard) {
-                secondCard = card;
-                card.classList.add("flipped");
-                img.src = `img/${randomNum}.gif`;
-                flippedCards.push(card);
-            }
-            
-            if (flippedCards.length === 2) {
-                const firstCardImg = flippedCards[0].querySelector("img").src;
-                const secondCardImg = flippedCards[1].querySelector("img").src;
-                
-                if (firstCardImg === secondCardImg) {
-                    flippedCards.forEach(card => {
-                        card.removeEventListener("click");
-                    });
-                    firstCard = null;
-                    secondCard = null;
-                    flippedCards = [];
-                } else {
-                    setTimeout(() => {
-                        flippedCards.forEach(card => {
-                            card.classList.remove("flipped");
-                            card.querySelector("img").src = "img/back.png";
-                        });
-                        flippedCards = [];
-                        firstCard = null;
-                        secondCard = null;
-                    }, 1000);
-                }
-            }
-        } else if (card.classList.contains("flipped")) {
-            card.classList.remove("flipped");
-            img.src = "img/back.png";
-            flippedCards.splice(flippedCards.indexOf(card), 1);
+    if (flippedCards.length < 2 && !card.classList.contains("flipped")) {
+      if (!firstCard) {
+        firstCard = card;
+        card.classList.add("flipped");
+        // Adiciona imagem aleatória apenas se a carta ainda não foi revelada antes
+        if (!card.dataset.img) {
+          card.dataset.img = `img/${Math.floor(Math.random() * 8) + 1}.gif`;
         }
-    });
+        img.src = card.dataset.img;
+        flippedCards.push(card);
+      }
+      else if (!secondCard) {
+        secondCard = card;
+        card.classList.add("flipped");
+        // Adiciona imagem aleatória apenas se a carta ainda não foi revelada antes
+        if (!card.dataset.img) {
+          card.dataset.img = `img/${Math.floor(Math.random() * 8) + 1}.gif`;
+        }
+        img.src = card.dataset.img;
+        flippedCards.push(card);
+      }
+      
+      if (flippedCards.length === 2) {
+        const firstCardImg = flippedCards[0].querySelector("img").src;
+        const secondCardImg = flippedCards[1].querySelector("img").src;
+                
+        if (firstCardImg === secondCardImg) {
+          flippedCards.forEach(card => {
+            card.removeEventListener("click");
+          });
+          firstCard = null;
+          secondCard = null;
+          flippedCards = [];
+        } else {
+          setTimeout(() => {
+            flippedCards.forEach(card => {
+              card.classList.remove("flipped");
+              card.querySelector("img").src = "img/back.png";
+            });
+            flippedCards = [];
+            firstCard = null;
+            secondCard = null;
+          }, 1000);
+        }
+      }
+    } else if (card.classList.contains("flipped")) {
+      card.classList.remove("flipped");
+      img.src = "img/back.png";
+      flippedCards.splice(flippedCards.indexOf(card), 1);
+    }
+  });
 });
-
