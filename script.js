139efe9ba790b76492bf5ce350ee1d8740c932e3
levymc <levymcruz@gmail.com>
2023-04-04 18:43:35 -0300

    removendo espaços vazios

--- script.js
+++ script.js
@@ -54,31 +54,3 @@ cards.forEach(card => {
     });
 });
 
-
-
-
-
-
-// cards.forEach(card => {
-//     card.addEventListener('click', () => {
-//         if (!firstCard) {
-//             firstCard = card;
-//             firstCard.children[0].classList.add('visible');
-//         } else if (!secondCard) {
-//             secondCard = card;
-//             secondCard.children[0].classList.add('visible');
-            
-//             if (firstCard.children[0].src === secondCard.children[0].src) {
-//                 firstCard = null;
-//                 secondCard = null;
-//             } else {
-//                 setTimeout(() => {
-//                     firstCard.children[0].classList.remove('visible');
-//                     secondCard.children[0].classList.remove('visible');
-//                     firstCard = null;
-//                     secondCard = null;
-//                 }, 1000);
-//             }
-//         }
-//     });
-// });
